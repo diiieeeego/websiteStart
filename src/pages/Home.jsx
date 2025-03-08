38f8128d9d509d4cd06844a95758866dcf6156b8
Jbilac <79338162+jbilac1@users.noreply.github.com>
2025-03-08 17:46:13 +0100

    lucide

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 
 import H1 from "../components/H1";
 import H2 from "../components/H2";
+import {Sun} from "lucide-react"
 
 const Home = () => {
   return (
     <div className="space-y-8">
-      <H1>Home</H1>
+      <H1>Home <Sun size={32} /></H1>
       <H2>Lorem ipsum dolor sit amet consectetur adipisicing elit</H2>
       <p>
         Odio iusto obcaecati libero maiores quos maxime dolorem esse culpa
